a1f5a62417e72e22ffd983ddafebc8f74d443d47
John McC <webhuddle@gmail.com>
2025-11-18 13:17:17 -0800

    listen on any interface

--- packages/poocam-core/src/poocam_core/random_sensor_process.py
+++ packages/poocam-core/src/poocam_core/random_sensor_process.py
@@ -4,7 +4,7 @@ from poocam_core.random_sensor import RandomSensor
 from poocam_core.sensor_data_source import SensorDataSource
 
 def start_server():
-    sensor_data_source: SensorDataSource = RandomSensor(8, 8, 10.0, 35.0, host="127.0.0.1", port=9090)
+    sensor_data_source: SensorDataSource = RandomSensor(8, 8, 10.0, 35.0, host="0.0.0.0", port=9090)
     sensor_data_source.serve()
 
 if __name__ == "__main__":
