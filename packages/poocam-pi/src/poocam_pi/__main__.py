07c99ead341f73dec04d1b5f9827c45e8e7f5272
John McC <webhuddle@gmail.com>
2025-12-11 19:31:56 -0800

    16x smoothing

--- packages/poocam-pi/src/poocam_pi/__main__.py
+++ packages/poocam-pi/src/poocam_pi/__main__.py
@@ -4,7 +4,7 @@ from poocam_core.sensor_data_source import SensorDataSource
 from poocam_pi.grid_eye_pi import GridEyeSensor
 
 def start_server():
-    sensor_data_source: SensorDataSource = GridEyeSensor(zoom=2)
+    sensor_data_source: SensorDataSource = GridEyeSensor(zoom=16)
     sensor_data_source.serve()
 
 if __name__ == "__main__":
