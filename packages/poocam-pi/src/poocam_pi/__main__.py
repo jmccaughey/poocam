1dede8263351899b830a02ee1f970457f649a67a
John McC <webhuddle@gmail.com>
2026-02-06 17:46:21 -0800

    disable zoom

--- packages/poocam-pi/src/poocam_pi/__main__.py
+++ packages/poocam-pi/src/poocam_pi/__main__.py
@@ -4,8 +4,8 @@ from poocam_core.sensor_data_source import SensorDataSource
 from poocam_pi.grid_eye_pi import GridEyeSensor
 
 def start_server():
-    sensor_data_source: SensorDataSource = GridEyeSensor(zoom=16)
+    sensor_data_source: SensorDataSource = GridEyeSensor(zoom=1)
     sensor_data_source.serve()
 
 if __name__ == "__main__":
-    start_server()  
+    start_server()
